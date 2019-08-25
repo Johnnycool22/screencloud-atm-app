5eee03c714fd0442246f1163a2d5d2119bbf86e6
John F <jforsythe13@qub.ac.uk>
2019-08-25 15:14:42 +0100

    component routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { AuthComponent } from './auth/auth.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AtmWithdrawComponent } from './atm-withdraw/atm-withdraw.component';
+import { BalanceComponent } from './atm-withdraw/balance/balance.component';
+import { CashWithdrawalComponent } from './atm-withdraw/cash-withdrawal/cash-withdrawal.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'auth', component: AuthComponent },
-  { path: 'atm-withdraw', component: AtmWithdrawComponent }
+  { path: 'atm-withdraw', component: AtmWithdrawComponent },
+  { path: 'balance', component: BalanceComponent },
+  { path: 'cash-withdrawal', component: CashWithdrawalComponent },
 ];
 
 @NgModule({
