7ffe52e543a71c8519a77a961ec362d9484cb6a0
John F <jforsythe13@qub.ac.uk>
2019-08-26 12:06:31 +0100

    javascript withdrawal file created

---
+++ src/app/withdrawal.js
@@ -0,0 +1,12 @@
+balance = 220
+
+if ( balance <= 220 )
+alert ("Wait, your request is processing");
+else alert ("You have insufficient funds in your account");
+
+function withdrawal() {
+    var currentBlance = 220;
+    if (currentBlance >= 0) {
+
+    }
+}
