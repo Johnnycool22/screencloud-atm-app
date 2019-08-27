4a86cdbc5219626635d954a02b95c0a020c4066e
John F <jforsythe13@qub.ac.uk>
2019-08-27 20:18:01 +0100

    css changes to header and background colour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name = 'app-root';
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.setStyle(document.body, 'background-color', 'gold');
+  }
 }
